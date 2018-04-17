de42add824b9694c0b868c7149b14a0b5784fe25
Cole Hart <cole@colehart.com>
2018-04-17 09:43:24 -0600

    setting up Employee intialize

--- test/employee_test.rb
+++ test/employee_test.rb
@@ -3,8 +3,17 @@ require './lib/employee'
 
 class EmployeeTest < Minitest::Test
   def test_instantiation
+    employee = Employee.new
+    assert_instance_of Employee, employee
   end
 
   def test_attributes
+    attrs = { employee_id: '5'
+              name: 'Sally Jackson'
+              role: 'Engineer'
+              start_date:'2015-01-01'
+              end_date: '2018-01-01' }
+    employee = Employee.new(employee_id, name, role, start_date, end_date)
+    assert_equal 5, employee.employee_id
   end
 end
